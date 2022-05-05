96de2cefe71909f241768fe9017d2701c88ff208
Joanarfc <joanarfcarvalho@hotmail.com>
2022-05-05 23:10:35 +0100

    Added array to store the playlist with songs and the logic to display a message according to the number of songs in the playlist

---
+++ Playlist/Playlist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] summerPlaylist;
+
+            summerPlaylist = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles"};
+            AmountSongs(summerPlaylist);
+        }
+
+        static void AmountSongs(string[] playlist)
+        {
+            int playlistLength = playlist.Length;
+
+            if(playlistLength == 8)
+            {
+                Console.WriteLine("summerStrut Playlist is ready to go!");
+            }
+            else if(playlistLength > 8)
+            {
+                Console.WriteLine("Too many songs!");
+
+            }
+            else
+            {
+                Console.WriteLine("Please add some songs!");
+
+            }
+
+        }
+    }
+}
